60e0b0bc8c02c9ff34f53d081a4f24eb7c4ac777
Mark Vincze <mvincze@travix.com>
2016-01-16 18:13:22 +0100

    Benchmark implementation

---
+++ src/CouchbasePerfTest/JsonAndGZipSerializer.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using Couchbase.Core.Serialization;
+using Newtonsoft.Json;
+
+namespace CouchbasePerfTest
+{
+    /// <summary>
+    /// A custom <see cref="ITypeSerializer"/> for the Couchbase .NET SDK that uses Json.NET and GZip compression for serialization.
+    /// The implementation is based on the one found here: http://blog.couchbase.com/2015/june/using-jil-for-custom-json-serialization-in-the-couchbase-.net-sdk
+    /// </summary>
+    public class JsonAndGZipSerializer : ITypeSerializer
+    {
+        private readonly JsonSerializer jsonSerializer;
+
+        public JsonAndGZipSerializer()
+        {
+            this.jsonSerializer = new JsonSerializer();
+        }
+
+        /// <summary>
+        /// Deserializes the specified stream into the <see cref="T:System.Type" /> T specified as a generic parameter.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="T:System.Type" /> specified as the type of the value.</typeparam>
+        /// <param name="stream">The stream.</param>
+        /// <returns>
+        /// The <see cref="T:System.Type" /> instance representing the value of the key.
+        /// </returns>
+        public T Deserialize<T>(Stream stream)
+        {
+            using (var decompressedStream = Decompress(stream))
+            {
+                using (var sr = new StreamReader(decompressedStream))
+                {
+                    using (var jtr = new JsonTextReader(sr))
+                    {
+                        return jsonSerializer.Deserialize<T>(jtr);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Deserializes the specified buffer into the <see cref="T:System.Type" /> T specified as a generic parameter.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="T:System.Type" /> specified as the type of the value.</typeparam>
+        /// <param name="buffer">The buffer to deserialize from.</param>
+        /// <param name="offset">The offset of the buffer to start reading from.</param>
+        /// <param name="length">The length of the buffer to read from.</param>
+        /// <returns>
+        /// The <see cref="T:System.Type" /> instance representing the value of the key.
+        /// </returns>
+        public T Deserialize<T>(byte[] buffer, int offset, int length)
+        {
+            T value = default(T);
+            if (length == 0)
+            {
+                return value;
+            }
+
+            using (var ms = new MemoryStream(buffer, offset, length))
+            {
+                using (var decompressedStream = Decompress(ms))
+                {
+                    using (var sr = new StreamReader(decompressedStream))
+                    {
+                        using (var jtr = new JsonTextReader(sr))
+                        {
+                            // Use the following code block only for value types.
+                            // Strangely enough Nullable<T> itself is a value type so we need to filter it out.
+                            if (typeof(T).IsValueType && (!typeof(T).IsGenericType
+                                                           || typeof(T).GetGenericTypeDefinition() != typeof(Nullable<>)))
+                            {
+                                // We can't declare Nullable<T> because T is not restricted to struct in this method scope.
+                                var type = typeof(Nullable<>).MakeGenericType(typeof(T));
+
+                                object nullableVal = jsonSerializer.Deserialize(jtr, type);
+
+                                // Either we have a null or an instance of Nullabte<T> that can be cast directly to T.
+                                value = nullableVal == null ? default(T) : (T)nullableVal;
+                            }
+                            else
+                            {
+                                value = jsonSerializer.Deserialize<T>(jtr);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Serializes the specified object into a buffer.
+        /// </summary>
+        /// <param name="obj">The object to serialize.</param>
+        /// <returns>
+        /// A <see cref="T:System.Byte" /> array that is the serialized value of the key.
+        /// </returns>
+        public byte[] Serialize(object obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                using (var sw = new StreamWriter(ms))
+                {
+                    using (var jtw = new JsonTextWriter(sw))
+                    {
+                        jsonSerializer.Serialize(jtw, obj);
+                        sw.Flush();
+
+                        ms.Position = 0;
+                        return Compress(ms);
+                    }
+                }
+            }
+        }
+
+        private byte[] Compress(Stream stream)
+        {
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                {
+                    stream.CopyTo(gs);
+                }
+
+                return mso.ToArray();
+            }
+        }
+
+        private Stream Decompress(Stream compressed)
+        {
+            var mso = new MemoryStream();
+
+            using (var gs = new GZipStream(compressed, CompressionMode.Decompress))
+            {
+                gs.CopyTo(mso);
+            }
+
+            mso.Position = 0;
+
+            return mso;
+        }
+    }
+}
---
+++ src/CouchbasePerfTest/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Couchbase;
+using Couchbase.Configuration.Client;
+using Couchbase.Core;
+using Newtonsoft.Json;
+
+namespace CouchbasePerfTest
+{
+    class Program
+    {
+        private static readonly string testDocument = File.ReadAllText(@"test-document.json");
+
+        private const int itemCount = 1000;
+
+        static void Main(string[] args)
+        {
+            DoPerfTest(false, "withoutcompression");
+            DoPerfTest(true, "withcompression");
+        }
+
+        private static void DoPerfTest(bool useCompression, string bucketName)
+        {
+            var cluster = useCompression ?
+                CreateClusterWithCompressionSerializer() : CreateClusterWithDefaultSerializer();
+
+            var bucket = cluster.OpenBucket(bucketName);
+
+            var manager = bucket.CreateManager("Administrator", "password");
+            manager.Flush();
+
+            var testObject = JsonConvert.DeserializeObject<RootObject>(testDocument);
+
+            var totalReadTime = TimeSpan.Zero;
+            var totalWriteTime = TimeSpan.Zero;
+
+            var swTotal = Stopwatch.StartNew();
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                var key = Guid.NewGuid().ToString();
+
+                var swRead = Stopwatch.StartNew();
+                bucket.Insert(new Document<RootObject>
+                {
+                    Content = testObject,
+                    Id = key
+                });
+                swRead.Stop();
+                totalReadTime += swRead.Elapsed;
+
+                var swWrite = Stopwatch.StartNew();
+                var result = bucket.Get<RootObject>(key);
+                swWrite.Stop();
+                totalWriteTime += swWrite.Elapsed;
+            }
+
+            swTotal.Stop();
+
+            Console.WriteLine("Total time elapsed: {0}", swTotal.Elapsed);
+            Console.WriteLine("Avg write time: {0}", TimeSpan.FromMilliseconds(totalWriteTime.TotalMilliseconds / itemCount));
+            Console.WriteLine("Avg read time: {0}", TimeSpan.FromMilliseconds(totalReadTime.TotalMilliseconds / itemCount));
+        }
+
+        private static ICluster CreateClusterWithDefaultSerializer()
+        {
+            return new Cluster();
+        }
+
+        private static ICluster CreateClusterWithCompressionSerializer()
+        {
+            return new Cluster(new ClientConfiguration
+            {
+                Serializer = () => new JsonAndGZipSerializer()
+            });
+        }
+    }
+}
